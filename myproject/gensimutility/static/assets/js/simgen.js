08571f31347407d196dd7ec2879b991a3cfe8be7
Sankalp2002 <53993946+Sankalp2002@users.noreply.github.com>
2021-03-12 03:19:29 +0530

    Update simgen.js

--- myproject/gensimutility/static/assets/js/simgen.js
+++ myproject/gensimutility/static/assets/js/simgen.js
@@ -2,27 +2,97 @@ function showresult() {
   window.open("result.html", "_blank");
 }
 
-function chose(){
-  var ele=document.getElementById('dsName');
-  var box=document.getElementById('urlbox1');
-  box.style.display = 'none';
-  document.getElementById('choosen').value = ele.innerText;
+function getSelectedOption(sel) {
+  var opt;
+  for ( var i = 0, len = sel.options.length; i < len; i++ ) {
+      opt = sel.options[i];
+      if ( opt.selected === true ) {
+          break;
+      }
+  }
+  return opt;
+}
+
+function dtoggle1(){
+  var ub1=document.getElementById('urlbox1');
+  if (ub1.style.display === "block") {
+    ub1.style.display = "none";
+  } 
+  var dd1=document.getElementById('ddone');
+  if (dd1.style.display === "block") {
+    dd1.style.display = "none";
+  } else {
+    dd1.style.display = "block";
+  }
   
 }
 
-function chose2(){
-  var ele=document.getElementById('dsName2');
-  var box=document.getElementById('urlbox2');
-  box.style.display = 'none';
-  document.getElementById('choosen2').value = ele.innerText;
+function utoggle1(){
+  var dd1=document.getElementById('ddone');
+  if (dd1.style.display === "block") {
+    dd1.style.display = "none";
+  } 
+  var ub1=document.getElementById('urlbox1');
+  if (ub1.style.display === "block") {
+    ub1.style.display = "none";
+  } else {
+    ub1.style.display = "block";
+  }
+  
+}
+
+function dtoggle2(){
+  var ub2=document.getElementById('urlbox2');
+  if (ub2.style.display === "block") {
+    ub2.style.display = "none";
+  } 
+  var dd2=document.getElementById('ddtwo');
+  if (dd2.style.display === "block") {
+    dd2.style.display = "none";
+  } else {
+    dd2.style.display = "block";
+  }
+  
+}
+
+function utoggle2(){
+  var dd2=document.getElementById('ddtwo');
+  if (dd2.style.display === "block") {
+    dd2.style.display = "none";
+  } 
+  var ub2=document.getElementById('urlbox2');
+  if (ub2.style.display === "block") {
+    ub2.style.display = "none";
+  } else {
+    ub2.style.display = "block";
+  }
+  
 }
 
 function confirm1(){
-  var ele=document.getElementById('dsName');
-  document.getElementById('dset1').value = ele.innerText;
+  var ele1=document.getElementById('pd1');
+  var ele2=document.getElementById('url1');
+  var div1=document.getElementById('ddone');
+    
+    if(div1.style.display == "block"){
+      document.getElementById('dset1').value = getSelectedOption(ele1).innerText;
+    }
+    else{
+      document.getElementById('dset1').value = ele2.value;
+    }
+
+
 }
 
 function confirm2(){
-  var ele=document.getElementById('dsName2');
-  document.getElementById('dset2').value = ele.innerText;
+  var ele1=document.getElementById('pd2');
+  var ele2=document.getElementById('url2');
+  var div1=document.getElementById('ddtwo');
+    
+  if(div1.style.display == "block"){
+    document.getElementById('dset2').value = getSelectedOption(ele1).innerText;
+  }
+  else{
+    document.getElementById('dset2').value = ele2.value;
+  }
 }
