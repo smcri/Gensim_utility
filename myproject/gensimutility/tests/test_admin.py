5df21e6e3bc1f09e1d50171c76e08966530b914d
byte-king <55686016+byte-king@users.noreply.github.com>
2021-04-06 22:03:01 +0530

    added invalid login

--- myproject/gensimutility/tests/test_admin.py
+++ myproject/gensimutility/tests/test_admin.py
@@ -20,7 +20,6 @@ class TestAdminPanel(TestCase):
         client.login(username=self.username, password=self.password)
         admin_pages = [
             "/admin/",
-            # put all the admin pages for your models in here.
             "/admin/auth/",
             "/admin/auth/group/",
             "/admin/auth/group/add/",
@@ -32,3 +31,32 @@ class TestAdminPanel(TestCase):
         for page in admin_pages:
             resp = client.get(page)
             assert resp.status_code == 200
+
+class TestAdminInvalid(TestCase):
+    def create_user(self):
+        self.username = "test_admin"
+        self.password = User.objects.make_random_password()
+        user, created = User.objects.get_or_create(username=self.username)
+        user.set_password(self.password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = False
+        user.save()
+        self.user = user
+
+    def test_spider_admin(self):
+        self.create_user()
+        client = Client()
+        client.login(username=self.username, password=self.password)
+        admin_pages = [            
+            "/admin/auth/",
+            "/admin/auth/group/",
+            "/admin/auth/group/add/",
+            "/admin/auth/user/",
+            "/admin/auth/user/add/",
+            "/admin/password_change/",
+            "/admin/logout/"
+        ]
+        for page in admin_pages:
+            resp = client.get(page)
+            assert resp.status_code == 302
